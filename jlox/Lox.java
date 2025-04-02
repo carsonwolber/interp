d7564b757e947ef35b94d4baa1009c125f2b881e
carsonwolber <ctw54@cornell.edu>
2025-04-01 23:52:28 -0400

    file path

--- jlox/Lox.java
+++ jlox/Lox.java
@@ -12,11 +12,16 @@ public class Lox {
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
-      System.exit(64); 
+      System.exit(64);
     } else if (args.length == 1) {
       runFile(args[0]);
     } else {
       runPrompt();
     }
+  }
+
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
   }
 }
