33f112733ea63d0b54d66294cb935b607e4d07f3
carsonwolber <ctw54@cornell.edu>
2025-04-01 23:56:45 -0400

    run quasi-implementation

--- jlox/Lox.java
+++ jlox/Lox.java
@@ -7,6 +7,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 public class Lox {
   public static void main(String[] args) throws IOException {
@@ -35,6 +36,16 @@ public class Lox {
       if (line == null)
         break;
       run(line);
+    }
+  }
+
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+
+    // For now, just print the tokens.
+    for (Token token : tokens) {
+      System.out.println(token);
     }
   }
 }
